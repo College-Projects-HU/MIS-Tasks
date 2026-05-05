eee3589d2c2902c319a4139c8721f1ece6d4fa08
Mostafa-Borham <borhamm56@gmail.com>
2026-05-05 15:43:17 +0300

    Update MongoDB task

--- MongoDB-Task/src/main/java/org/example/Main.java
+++ MongoDB-Task/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import com.mongodb.client.*;
 import org.bson.Document;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Updates.*;
@@ -15,60 +16,58 @@ public class Main {
         MongoClient client = MongoClients.create("mongodb://localhost:27017");
         MongoDatabase db = client.getDatabase("taskDB");
 
-        MongoCollection<Document> users = db.getCollection("users");
+        MongoCollection<Document> teachers = db.getCollection("teachers");
         MongoCollection<Document> courses = db.getCollection("courses");
 
         // CLEAN
-        users.drop();
+        teachers.drop();
         courses.drop();
 
-        // INSERT USERS
-        users.insertMany(Arrays.asList(
-                new Document("_id", 1).append("name", "Ali").append("courseIds", Arrays.asList(101, 102)),
-                new Document("_id", 2).append("name", "Sara").append("courseIds", Arrays.asList(101, 102)),
-                new Document("_id", 3).append("name", "Omar").append("courseIds", Arrays.asList(102, 101))
+        // INSERT
+        teachers.insertMany(Arrays.asList(
+                new Document("_id", 1).append("name", "Dr Ahmed"),
+                new Document("_id", 2).append("name", "Dr Sara"),
+                new Document("_id", 3).append("name", "Dr Omar")
         ));
 
-        // INSERT COURSES
         courses.insertMany(Arrays.asList(
-                new Document("_id", 101).append("title", "Math"),
-                new Document("_id", 102).append("title", "CS"),
-                new Document("_id", 103).append("title", "AI")
+                new Document("_id", 101).append("title", "Math").append("teacherId", 1),
+                new Document("_id", 102).append("title", "CS").append("teacherId", 1),
+                new Document("_id", 103).append("title", "AI").append("teacherId", 2),
+                new Document("_id", 104).append("title", "IS").append("teacherId", 3)
+
         ));
 
-        // DELETE
-        users.deleteOne(eq("_id", 3));
-        courses.deleteOne(eq("_id", 103));
+        // ================= DELETE =================
+        teachers.deleteOne(eq("_id", 3));
+        courses.deleteOne(eq("_id", 104));
 
         // ADD SCORE ARRAY
-        users.updateMany(new Document(), set("Score", Arrays.asList(1, 2, 3, 4)));
-        courses.updateMany(new Document(), set("Score", Arrays.asList(5, 6, 7, 8)));
+        teachers.updateMany(new Document(), set("Score", Arrays.asList(1, 2, 3, 4)));
+        courses.updateMany(new Document(), set("Score", Arrays.asList(10, 20, 30, 40)));
 
-        // CONDITIONAL UPDATE 
+        // CONDITIONAL UPDATE
+        // if id == 1 -> put 5 in position 3
+        teachers.updateOne(eq("_id", 1),
+                pushEach("Score", Arrays.asList(5),
+                        new com.mongodb.client.model.PushOptions().position(2)));
 
-        // users: id = 1 -> add 5 in index 2
-        users.updateOne(eq("_id", 1),
-                pushEach("Score", Arrays.asList(5), new com.mongodb.client.model.PushOptions().position(2)));
-
-        // users: others -> add 6 in index 3
-        users.updateMany(ne("_id", 1),
-                pushEach("Score", Arrays.asList(6), new com.mongodb.client.model.PushOptions().position(3)));
-
-        // courses: id = 101 -> add 5
         courses.updateOne(eq("_id", 101),
-                pushEach("Score", Arrays.asList(5), new com.mongodb.client.model.PushOptions().position(2)));
+                pushEach("Score", Arrays.asList(5),
+                        new com.mongodb.client.model.PushOptions().position(2)));
 
-        // courses: others -> add 6
+        // else → put 6 in position 4
+        teachers.updateMany(ne("_id", 1),
+                pushEach("Score", Arrays.asList(6),
+                        new com.mongodb.client.model.PushOptions().position(3)));
+
         courses.updateMany(ne("_id", 101),
-                pushEach("Score", Arrays.asList(6), new com.mongodb.client.model.PushOptions().position(3)));
+                pushEach("Score", Arrays.asList(6),
+                        new com.mongodb.client.model.PushOptions().position(3)));
 
-        //  MULTIPLY * 20
-
-        users.updateMany(new Document(),
-                mul("Score.$[]", 20));
-
-        courses.updateMany(new Document(),
-                mul("Score.$[]", 20));
+        // MULTIPLY SCORE BY 20
+        teachers.updateMany(new Document(), mul("Score.$[]", 20));
+        courses.updateMany(new Document(), mul("Score.$[]", 20));
 
         System.out.println("MongoDB Task Completed");
     }
